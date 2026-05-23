ffae4853961ec2ea56f11e063023e7b8962b4809
Your Name <your-email@gmail.com>
2026-05-23 23:39:14 +0600

    fix: sameSite none cookie

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://sports-sphere-client-phi.vercel.app',
+];
+
 app.use(
   cors({
-    origin: [
-      'http://localhost:3000',
-      'https://sports-sphere-client-phi.vercel.app',
-    ],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   }),
 );
@@ -28,29 +37,31 @@ const client = new MongoClient(process.env.MONGODB_URI);
 async function startServer() {
   try {
     await client.connect();
-
     console.log('✅ Connected to MongoDB');
 
     const db = client.db('Sports-Sphere');
 
     const auth = betterAuth({
       database: mongodbAdapter(db),
-
       emailAndPassword: {
         enabled: true,
       },
-
       baseURL: process.env.BETTER_AUTH_URL,
-
       secret: process.env.BETTER_AUTH_SECRET,
-
-      trustedOrigins: [
-        'http://localhost:3000',
-        'https://sports-sphere-client-phi.vercel.app',
-      ],
+      trustedOrigins: allowedOrigins,
+      advanced: {
+        crossSubdomainCookies: {
+          enabled: true,
+        },
+        defaultCookieAttributes: {
+          secure: true,
+          httpOnly: true,
+          sameSite: 'none',
+          partitioned: true,
+        },
+      },
     });
 
-    // FIXED ROUTE
     app.use('/api/auth', toNodeHandler(auth));
 
     app.get('/', (req, res) => {
